d6836e00ae4f6173284d233b1db1351a07fb1bf5
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2012-06-28 14:31:51 -0400

    WIP

--- devderby/js/jquery.sidebar.js
+++ devderby/js/jquery.sidebar.js
@@ -24,10 +24,45 @@ jQuery(function() {
   
   // other layout elements
   jQuery("#volumerendering").button();
-  jQuery("#volumerendering").css('font-size', '10px');
+  jQuery("#volumerendering").unbind('mouseenter').unbind('mouseleave');
+  jQuery("#volumerendering").click(function() {
+
+    jQuery("#slicing").removeClass('ui-state-active');
+    jQuery("#volumerendering").addClass('ui-state-active');
+    
+  });
+  jQuery("#slicing").button();
+  jQuery("#slicing").addClass('ui-state-active');
+  jQuery("#slicing").unbind('mouseenter').unbind('mouseleave');
+  jQuery("#slicing").click(function() {
+
+    jQuery("#volumerendering").removeClass('ui-state-active');
+    jQuery("#slicing").addClass('ui-state-active');
+    
+  });
+  jQuery("#modes").buttonset();
+  
+  jQuery(".color-picker").miniColors({
+    letterCase: 'uppercase',
+    change: function(hex, rgb) {
+
+      logData(hex, rgb);
+    }
+  });
+  
+
+  jQuery('#inverted').button();
+  
+  jQuery('#color2').button();
+  
+  jQuery("#colormodes").buttonset();
+  jQuery('#inverted').removeClass('ui-corner-left').addClass('ui-corner-top');
+  jQuery('#color2').removeClass('ui-corner-right').addClass('ui-corner-bottom');
   
   jQuery("#threshold-volume").slider();
   
+
+
   // customize slider look and feeld
   jQuery('.ui-slider').height(5);
   jQuery('.ui-slider-handle').height(10);
@@ -37,6 +72,8 @@ jQuery(function() {
   // jQuery('.ui-widget').css('letter-spacing', '1px');
   jQuery('.ui-corner-all').css('-moz-border-radius', '2px');
   jQuery('.ui-corner-all').css('-webkit-border-radius', '2px');
+  
+  jQuery('.ui-button').css('padding', '2px 2px 0px');
   
 
 });
