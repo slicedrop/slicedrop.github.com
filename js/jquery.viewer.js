9d002aee11524add604383e2cc8ba69fd94b05a9
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2012-09-23 22:31:40 -0400

    Testing a split screen feature.

--- js/jquery.viewer.js
+++ js/jquery.viewer.js
@@ -1,22 +1,35 @@
 // the viewers
 jQuery(function() {
 
-  
-
   // create the 2d sliders
   jQuery("#yellow_slider").slider({
-    slide: volumeslicingX
+    slide : volumeslicingX
   });
   jQuery("#yellow_slider .ui-slider-handle").unbind('keydown');
-  
+
   jQuery("#red_slider").slider({
-    slide: volumeslicingY
+    slide : volumeslicingY
   });
   jQuery("#red_slider .ui-slider-handle").unbind('keydown');
-  
+
   jQuery("#green_slider").slider({
-    slide: volumeslicingZ
+    slide : volumeslicingZ
   });
   jQuery("#green_slider .ui-slider-handle").unbind('keydown');
-  
+
 });
+
+// splits the document into two frames
+function split_screen() {
+
+  if ( parent.viewer_left !== undefined ) {
+    // exit if we are already in split screen mode
+    return;
+  }
+
+  frameset = '<FRAMESET cols="50%,50%" BORDER=3>' + '<FRAME src="'
+      + document.location.href + '"' + ' NAME="viewer_left">' + '<FRAME src="'
+      + document.location.href + '"' + ' NAME="viewer_right">' + '</FRAMESET>';
+  location = 'javascript:frameset';
+
+};
