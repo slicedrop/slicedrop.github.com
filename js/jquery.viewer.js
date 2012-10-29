195906b17e88e865c3ceb7766fa0fffbe5a77721
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2012-10-29 13:20:29 -0400

    Add appropriate LICENSE header to all relevant code files.

--- js/jquery.viewer.js
+++ js/jquery.viewer.js
@@ -1,3 +1,32 @@
+/*
+
+    .----.                    _..._                                                     .-'''-.                           
+   / .--./    .---.        .-'_..._''.                          _______                '   _    \                         
+  ' '         |   |.--.  .' .'      '.\     __.....__           \  ___ `'.           /   /` '.   \_________   _...._      
+  \ \         |   ||__| / .'            .-''         '.    ,.--. ' |--.\  \         .   |     \  '\        |.'      '-.   
+   `.`'--.    |   |.--.. '             /     .-''"'-.  `. //    \| |    \  ' .-,.--.|   '      |  '\        .'```'.    '. 
+     `'-. `.  |   ||  || |            /     /________\   \\\    /| |     |  '|  .-. \    \     / /  \      |       \     \
+         `. \ |   ||  || |            |                  | `'--' | |     |  || |  | |`.   ` ..' /    |     |        |    |
+           \ '|   ||  |. '            \    .-------------' ,.--. | |     ' .'| |  | |   '-...-'`     |      \      /    . 
+            | |   ||  | \ '.          .\    '-.____...---.//    \| |___.' /' | |  '-                 |     |\`'-.-'   .'  
+            | |   ||__|  '. `._____.-'/ `.             .' \\    /_______.'/  | |                     |     | '-....-'`    
+           / /'---'        `-.______ /    `''-...... -'    `'--'\_______|/   | |                    .'     '.             
+     /...-'.'                       `                                        |_|                  '-----------'           
+    /--...-'                                                                                                              
+    
+    Slice:Drop - Instantly view scientific and medical imaging data in 3D.
+    
+     http://slicedrop.com
+     
+    Copyright (c) 2012 The Slice:Drop and X Toolkit Developers <dev@goXTK.com>
+    
+    Slice:Drop is licensed under the MIT License:
+      http://www.opensource.org/licenses/mit-license.php    
+      
+    CREDITS: http://slicedrop.com/LICENSE
+     
+*/
+
 // the viewers
 jQuery(function() {
 
