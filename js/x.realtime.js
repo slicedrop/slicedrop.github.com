f94fe8af434a20bfc3442c2477fd2f08a6dd6fa5
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2013-03-04 16:51:42 -0500

    Sync all UI elements back when receiving a socket message.

--- js/x.realtime.js
+++ js/x.realtime.js
@@ -35,13 +35,60 @@ RT.link = function() {
       if (_data.volume.file.length == 0) return;
       
       volume[data.target] = data.value;
+      
+      // propagate back to UI
+      jQuery("#yellow_slider").slider("option", "value", volume.indexX);
+      jQuery("#red_slider").slider("option", "value", volume.indexY);
+      jQuery("#green_slider").slider("option", "value", volume.indexZ);
 
+      if (volume.volumeRendering) {
+        jQuery('#slicing').removeClass('ui-state-active');
+        jQuery('#volumerendering').addClass('ui-state-active');
+        jQuery('#windowlevel-label').hide();
+        jQuery('#windowlevel-volume').hide();
+        jQuery('#opacity-label').show();
+        jQuery('#opacity-volume').show();
+      } else {
+        jQuery('#slicing').addClass('ui-state-active');
+        jQuery('#volumerendering').removeClass('ui-state-active');
+        jQuery('#windowlevel-label').show();
+        jQuery('#windowlevel-volume').show();
+        jQuery('#opacity-label').hide();
+        jQuery('#opacity-volume').hide();
+      }
+      
+      jQuery('#opacity-volume').slider("option", "value", volume.opacity * 100);          
+      jQuery('#threshold-volume').dragslider("option", "values", [volume.lowerThreshold, volume.upperThreshold]);
+      jQuery('#windowlevel-volume').dragslider("option", "values", [volume.windowLow, volume.windowHigh]);
+      
+      var bgColor = ((1 << 24) + (volume.minColor[0] * 255 << 16) +
+          (volume.minColor[1] * 255 << 8) + volume.minColor[2] * 255)
+          .toString(16).substr(1);
+      
+      var fgColor = ((1 << 24) + (volume.maxColor[0] * 255 << 16) +
+          (volume.maxColor[1] * 255 << 8) + volume.maxColor[2] * 255)
+          .toString(16).substr(1);
+      
+      jQuery('#bgColorVolume').miniColors("value", bgColor);
+      jQuery('#fgColorVolume').miniColors("value", fgColor);      
+      
+      
     });
     RT._link.bind('client-labelmap-sync', function(data) {
 
       if (_data.labelmap.file.length == 0) return;
       
       volume.labelmap[data.target] = data.value;
+      
+      // propagate back to UI
+      if (!volume.labelmap.visible) {
+        $('#labelmapvisibility').removeClass('show-icon');
+        $('#labelmapvisibility').addClass('hide-icon');
+      } else {
+        $('#labelmapvisibility').addClass('show-icon');
+        $('#labelmapvisibility').removeClass('hide-icon');
+      }
+      jQuery('#opacity-labelmap').slider("option", "value", volume.labelmap.opacity * 100);
 
     });    
     RT._link.bind('client-mesh-sync', function(data) {
@@ -50,19 +97,55 @@ RT.link = function() {
       
       mesh[data.target] = data.value;
 
+      // propagate back to UI
+      if (!mesh.visible) {
+        $('#meshvisibility').removeClass('show-icon');
+        $('#meshvisibility').addClass('hide-icon');          
+      } else {
+        $('#meshvisibility').addClass('show-icon');
+        $('#meshvisibility').removeClass('hide-icon');        
+      }      
+      jQuery('#opacity-mesh').slider("option", "value", mesh.opacity * 100);
+      var meshColor = ((1 << 24) + (mesh.color[0] * 255 << 16) +
+          (mesh.color[1] * 255 << 8) + mesh.color[2] * 255)
+          .toString(16).substr(1);        
+      jQuery('#meshColor').miniColors("value", meshColor);
+      
     });        
     RT._link.bind('client-scalars-sync', function(data) {
 
       if (_data.scalars.file.length == 0) return;
       
       mesh.scalars[data.target] = data.value;
+      
+      // propagate back to UI
+      jQuery("#threshold-scalars").dragslider("option", "values",
+          [mesh.scalars.lowerThreshold * 100, mesh.scalars.upperThreshold * 100]);      
 
+      var scalarsminColor = ((1 << 24) + (mesh.scalars.minColor[0] * 255 << 16) +
+          (mesh.scalars.minColor[1] * 255 << 8) + mesh.scalars.minColor[2] * 255)
+          .toString(16).substr(1);        
+      jQuery('#scalarsMinColor').miniColors("value", scalarsminColor);
+              
+      var scalarsmaxColor = ((1 << 24) + (mesh.scalars.maxColor[0] * 255 << 16) +
+          (mesh.scalars.maxColor[1] * 255 << 8) + mesh.scalars.maxColor[2] * 255)
+          .toString(16).substr(1);        
+      jQuery('#scalarsMaxColor').miniColors("value", scalarsmaxColor);      
+      
     });    
     RT._link.bind('client-fibers-sync', function(data) {
 
       if (_data.fibers.file.length == 0) return;
       
       fibers[data.target] = data.value;
+      
+      if (!fibers.visible) {
+        $('#fibersvisibility').removeClass('show-icon');
+        $('#fibersvisibility').addClass('hide-icon');          
+      } else {
+        $('#fibersvisibility').addClass('show-icon');
+        $('#fibersvisibility').removeClass('hide-icon');
+      }      
 
     });    
     RT._link.bind('client-fibersscalars-sync', function(data) {
@@ -70,6 +153,7 @@ RT.link = function() {
       if (_data.fibers.file.length == 0) return;
       
       fibers.scalars[data.target] = data.value;
+      jQuery('#threshold-fibers').dragslider("option", "values", [fibers.scalars.lowerThreshold, fibers.scalars.upperThreshold]);
 
     });    
 
